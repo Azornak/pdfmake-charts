9f84fd95abf469de86f19f2da92fcca57ec7241d
Christoffer Anderen Træen <christoffer.andersen@live.com>
2021-08-11 11:00:39 +0200

    Fix displaying pdf on page

--- src/App.js
+++ src/App.js
@@ -5,37 +5,36 @@ import {
   lineChartDummyData,
   lineChartDummyData2,
 } from "./PDF/Charts/lineChartDummyData";
-import { useEffect } from "react";
+import { useEffect, useRef } from "react";
 import { getBarChart } from "./PDF/Charts/barchart";
 import { barChartDummyData } from "./PDF/Charts/barChartDummyData";
 function App() {
   console.log("Download PDF clicked");
-
+  const iframeRef = useRef(null);
   useEffect(() => {
-    const dataUrl = getLineChart(lineChartDummyData);
-    const dataUrl2 = getLineChart(lineChartDummyData2);
-    const dataUrl3 = getBarChart(barChartDummyData);
-  }, []);
+    const downloadPDF = async (pdfType) => {
+      const dataUrl = getLineChart(lineChartDummyData);
+      const dataUrl2 = getLineChart(lineChartDummyData2);
+      const dataUrl3 = getBarChart(barChartDummyData);
 
-  const downloadPDF = async (pdfType) => {
-    const dataUrl = getLineChart(lineChartDummyData);
-    const dataUrl2 = getLineChart(lineChartDummyData2);
-    const pdfData = {
-      heading: ["test1", "test2", "test3", "test4"],
-      imageTest: dataUrl.toBase64Image(),
-      chartData: [
-        { x: "label", y: 123 },
-        { x: "label", y: 123 },
-        { x: "label", y: 123 },
-        { x: "label", y: 123 },
-      ],
+      const pdfData = {
+        heading: ["test1", "test3", "test3", "test4"],
+        imageTest: dataUrl.toBase64Image(),
+        chartData: [
+          { x: "label", y: 123 },
+          { x: "label", y: 123 },
+          { x: "label", y: 123 },
+          { x: "label", y: 123 },
+        ],
+      };
+      pdfGen(pdfType, iframeRef.current, pdfData);
     };
-    pdfGen(pdfType, pdfData);
-  };
+    downloadPDF();
+  }, []);
 
   return (
     <div className="App">
-      <button onClick={() => downloadPDF("testpdf")}>Download PDF</button>
+      <iframe ref={iframeRef} style={{ width: "100vw", height: "100vh" }} />
     </div>
   );
 }
--- src/PDF/pdfgenerator.js
+++ src/PDF/pdfgenerator.js
@@ -1,20 +1,25 @@
-import testPDF from './documentDefinitions/testpdf';
-import pdfMake from 'pdfmake';
+import testPDF from "./documentDefinitions/testpdf";
+import pdfMake from "pdfmake";
 import pdfFonts from "pdfmake/build/vfs_fonts";
 pdfMake.vfs = pdfFonts.pdfMake.vfs;
-const pdfGenerator = (pdfType, data) => {
-    console.log("Generating PDF");
-    var dd = {};
-    switch(pdfType){
-        case 'testPDF':
-            dd = testPDF(data);
-            break;
-        default:
-            dd = testPDF(data);
-    }
-    console.log("Starting download");
-    pdfMake.createPdf(dd).download();
-}
 
+const pdfGenerator = (pdfType, iframe, data) => {
+  console.log("Generating PDF");
+  var dd = {};
+  switch (pdfType) {
+    case "testPDF":
+      dd = testPDF(data);
+      break;
+    default:
+      dd = testPDF(data);
+  }
+  //   console.log("Starting download");
+  //   pdfMake.createPdf(dd).download();
+
+  const pdfDoc = pdfMake.createPdf(dd);
+  pdfDoc.getDataUrl((dataUrl) => {
+    iframe.src = dataUrl;
+  });
+};
 
 export default pdfGenerator;
